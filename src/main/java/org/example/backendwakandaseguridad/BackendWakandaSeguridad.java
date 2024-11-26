1c799712d87dbaafa02cda97ef1c3f870cd1f964
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-11-26 19:19:57 +0100

    Conf-pom-Res-Docker

---
+++ src/main/java/org/example/backendwakandaseguridad/BackendWakandaSeguridad.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaseguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWakandaSeguridad {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWakandaSeguridad.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/backendwakandaseguridad/BackendWakandaSeguridadTests.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaseguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWakandaSeguridadTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
